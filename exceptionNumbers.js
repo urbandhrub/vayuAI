f3a2c7d645e8e9e02add1ae4008f419b42218aa6
urbandhrub <urbandhrub@gmail.com>
2026-05-01 13:17:55 +0530

    exceptionNumbers.js

--- exceptionNumbers.js
+++ exceptionNumbers.js
@@ -1,10 +1,8 @@
 // exceptionNumbers.js
-
-// Store numbers in E.164 without '+', include country code
 const ALLOWED_NUMBERS = new Set([
   "918240801921",
   "919330393298",
-  "916290209939",
+  "916290209939"
 ]);
 
 module.exports = ALLOWED_NUMBERS;
